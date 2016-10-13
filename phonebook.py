54225b03989a2d452cafcd85fc263b8e97e41671
Kirk Abbott <kirkabbott1@gmail.com>
2016-10-13 13:35:27 -0400

    updates phonebook db practice

--- phonebook.py
+++ phonebook.py
@@ -8,14 +8,14 @@ app = Flask('MyApp')
 def home():
     return render_template(
         'layout.html',
-        title='BOOK O'' PHONE',
+        title="BOOK O' PHONE",
     )
 @app.route('/list_o_entries')
 def list_entries():
     query = db.query('select * from phonebook')
     return render_template(
         'list_o_entries.html',
-        title='BOOK O'' PHONE',
+        title="BOOK O\' PHONE",
         phonebook_list=query.namedresult()
     )
 
@@ -25,16 +25,36 @@ def new_phonebook_form():
         'new_phonebook.html',
         title='Add Entry')
 
-@app.route('/submit_new_entry', methods=['POST'])
-def submit_form():
+@app.route('/update_entry')
+def update_entry():
+    id = int(request.args.get('id'))
+    query = db.query('''
+    select * from phonebook
+    where id = %d''' % id)
+    entry = query.namedresult()[0]
+    return render_template(
+        'update_entry.html',
+        entry=entry
+    )
+
+@app.route('/submit_update_entry', methods=['POST'])
+def submit_update_entry():
+    id = int(request.form.get('id'))
     name = request.form.get('name')
     phone_number = request.form.get('phone_number')
     email = request.form.get('email')
-    db.insert(
-        'phonebook',
-        name=name,
-        phone_number=phone_number,
-        email=email)
+    action = request.form.get('action')
+    if action == 'update':
+        db.update('phonebook', {
+            'id': id,
+            'name': name,
+            'phone_number': phone_number,
+            'email': email
+        })
+    elif action == 'delete':
+        db.delete('phonebook', { 'id': id })
+    else:
+        raise Exception("I don know how to %s" % action)
     return redirect('/')
 
 if __name__ == '__main__':
